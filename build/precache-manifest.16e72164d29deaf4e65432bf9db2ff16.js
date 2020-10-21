4c13c8db9a77f045f220868c8b95ee626f696f18
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-10-21 08:47:42 -0400

    updated resume

---
+++ build/precache-manifest.16e72164d29deaf4e65432bf9db2ff16.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b08a0c060bf2d0228fae5200aea7f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93096a7d8ec1459573a4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d2cd647e2d48483dadf6",
+    "url": "/static/css/main.298e1b65.chunk.css"
+  },
+  {
+    "revision": "93096a7d8ec1459573a4",
+    "url": "/static/js/2.b7fe56e5.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.b7fe56e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cd647e2d48483dadf6",
+    "url": "/static/js/main.472148e1.chunk.js"
+  },
+  {
+    "revision": "4d8b4c10938f10b816cb",
+    "url": "/static/js/runtime-main.44c95852.js"
+  },
+  {
+    "revision": "d1e40a6f818168da73cbc86540353786",
+    "url": "/static/media/framemotion-pic.d1e40a6f.jpg"
+  },
+  {
+    "revision": "92febe5fa8264230a11dd0ce6ac207ba",
+    "url": "/static/media/front-hero.92febe5f.jpg"
+  },
+  {
+    "revision": "4ff21ef59d8222f16d07317125bdec27",
+    "url": "/static/media/hair.4ff21ef5.png"
+  },
+  {
+    "revision": "389314a8904f6f6282654e1f85812e91",
+    "url": "/static/media/new-lambda-pic.389314a8.jpg"
+  },
+  {
+    "revision": "317e5566ab06755b7dc3ed254370b83c",
+    "url": "/static/media/pups-pic.317e5566.png"
+  },
+  {
+    "revision": "017add625de103dde742cdf12b15498e",
+    "url": "/static/media/rec-frame_generic_light.017add62.png"
+  },
+  {
+    "revision": "af4d30e9be4d2568fc397cf7c25fcbf9",
+    "url": "/static/media/receipt-pic.af4d30e9.png"
+  },
+  {
+    "revision": "b62f7245ad8c7c21c7429ef2d9de16c4",
+    "url": "/static/media/rent-frame_generic_light.b62f7245.png"
+  },
+  {
+    "revision": "57d5be898b1064284d80819955115b5b",
+    "url": "/static/media/rent-recording.57d5be89.mp4"
+  },
+  {
+    "revision": "21c7df4fc848e5ab8b09ef7744b487b8",
+    "url": "/static/media/resume.21c7df4f.png"
+  },
+  {
+    "revision": "da78b42f8ee12aa18122da912f4275e1",
+    "url": "/static/media/resume.da78b42f.docx"
+  },
+  {
+    "revision": "1d7d1b4c54580b36eed7ec2c259568df",
+    "url": "/static/media/rev-frame_generic_light.1d7d1b4c.png"
+  },
+  {
+    "revision": "669d06fa71b1552354e46f1090ecfe80",
+    "url": "/static/media/rev-rec.669d06fa.mp4"
+  },
+  {
+    "revision": "2518df33007c06e989a60a2bb03195e4",
+    "url": "/static/media/rolly.2518df33.png"
+  },
+  {
+    "revision": "66044ac54278e46ed78ff9a875120958",
+    "url": "/static/media/script-tag.66044ac5.jpg"
+  },
+  {
+    "revision": "ad3a0c71d44cf4dbe20306f1f997b912",
+    "url": "/static/media/sick.ad3a0c71.jpg"
+  },
+  {
+    "revision": "9e98b68780e00290bc1336dbde478470",
+    "url": "/static/media/toolkit.9e98b687.jpg"
+  },
+  {
+    "revision": "dd8bcd37115c21207237597715e2ccf3",
+    "url": "/static/media/wow-frame_generic_light.dd8bcd37.png"
+  },
+  {
+    "revision": "1beb676fddbdcd06c16d9f406f37d691",
+    "url": "/static/media/wow-rec-frame_generic_light.1beb676f.png"
+  },
+  {
+    "revision": "51e7b30217d0c6e9ddc82bfec8d979eb",
+    "url": "/static/media/wow-rec.51e7b302.mp4"
+  }
+]);
