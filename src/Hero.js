49b80a266ac73f82edf457f5fa40efc0cef65a21
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-03-16 01:10:32 -0400

    added react scroll

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Hero.scss";
 import Cut from "./img/cutOut.png";
+import {Link} from 'react-scroll';
 
 const Hero = () => {
   return (
@@ -18,27 +19,111 @@ const Hero = () => {
           and I love what I do.
         </p>
 
-        <p className="chat">LET'S CHAT!</p>
+        <Link
+        activeClass="active"
+        to="contact"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><p className="chat">LET'S CHAT!</p></Link>
       </div>
       <div className="mid_header_right">
         <img src={Cut} alt="coder" />
       </div>
       <div className="tech_list">
         <ul className="list_one">
-          <li>ReactJS</li>
+        <Link
+        activeClass="active"
+        to="work"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>ReactJS</li></Link>
+        <Link
+        activeClass="active"
+        to="next"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>NextJS</li></Link>
 
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>JavaScript</li>
-          <li>LESS/SASS</li>
-          <li>Photoshop</li>
+<Link
+        activeClass="active"
+        to="html"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>HTML5</li></Link>
+          <Link
+        activeClass="active"
+        to="work"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>CSS3</li></Link>
+                 <Link
+        activeClass="active"
+        to="javascript"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>JavaScript</li></Link>
+                 <Link
+        activeClass="active"
+        to="sass"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>LESS/SASS</li></Link>
         </ul>
         <ul className="list_two">
-          <li>NodeJS</li>
-          <li>Express</li>
-          <li>SQL</li>
-          <li>noSQL</li>
-          <li>Git Version Control</li>
+        <Link
+        activeClass="active"
+        to="node"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>NodeJS</li></Link>
+                 <Link
+        activeClass="active"
+        to="express"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>Express</li></Link>
+                 <Link
+        activeClass="active"
+        to="sql"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>SQL</li></Link>
+                 <Link
+        activeClass="active"
+        to="nosql"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>noSQL</li></Link>
+                 <Link
+        activeClass="active"
+        to="git"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        ><li>Git Version Control</li></Link>
         </ul>
       </div>
     </section>
